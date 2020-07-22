370e9692e72151d37708d44a78d8d1689413f261
Ирина Бобина <bobin.ir495@gmail.com>
2020-07-23 02:07:46 +0300

    новое

--- homework.py
+++ homework.py
@@ -30,7 +30,7 @@ class Calculator:
         lim = self.limit
         todayremained = lim - todaysum
         return todayremained
-    
+
 
 class Record:
     def __init__(self, amount, comment, date=''):
@@ -45,10 +45,10 @@ class Record:
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_remained = self.today_remained()
-        
+
         if calories_remained > 0:
-            a = (f'Сегодня можно съесть что-нибудь ещё', 
-            ', но с общей калорийностью не более {calories_remained} кКал')
+            a = ('Сегодня можно съесть что-нибудь ещё, но' 
+                f' с общей калорийностью не более {calories_remained} кКал')
         else:
             a = 'Хватит есть!'
         return a
@@ -58,51 +58,62 @@ class CashCalculator(Calculator):
     EURO_RATE = 81.41
     USD_RATE = 71.21
 
+    currencies = {
+        'rub': 'руб',
+        'eur': 'Euro',
+        'usd': 'USD'
+    }
+
     def get_today_cash_remained(self, currency):
         cash_remained = self.today_remained()
-        
+
+        if currency not in CashCalculator.currencies:
+            M = 'Незнакомая валюта'
+            return M
+
+        today_currency = CashCalculator.currencies[currency]
+
         if currency == 'rub':
-            cur = 'руб'
             N = cash_remained
         elif currency == 'usd':
-            cur = 'USD'
             N = cash_remained/CashCalculator.USD_RATE
         elif currency == 'eur':
-            cur = 'Euro'
             N = cash_remained/CashCalculator.EURO_RATE
-        else:
-            return ('Незнакомая валюта')
-            
+
         if cash_remained > 0: 
-            return (f'На сегодня осталось {N:.2f} {cur}') 
+            return (f'На сегодня осталось {N:.2f} {today_currency}') 
         elif cash_remained == 0:
             return ('Денег нет, держись')
         else: 
-            return (f'Денег нет, держись: твой долг - {abs(N):.2f} {cur}'
+            K = abs(N)
+            return (f'Денег нет, держись:'
+            f' твой долг - {K:.2f} {today_currency}')
 
-
-cash_calculator = CashCalculator(1000)
-calories_calculator = CaloriesCalculator(2500)
+if __name__ == "__main__":
+    cash_calculator = CashCalculator(1000)
+    calories_calculator = CaloriesCalculator(2500)
  
-r2 = Record(amount=1545, comment='Корзина потр.', date='16.07.2020')
-r3 = Record(amount=791, comment='Катание на такси', date='15.07.2020')
+    r1 = Record(amount=145, comment="Безудержный шопинг", date="23.07.2020")
+    r2 = Record(amount=1545, comment="Корзина", date="23.07.2020")
+    r3 = Record(amount=791, comment="Катание на такси", date="22.07.2020")
  
-cash_calculator.add_record(r1)
-cash_calculator.add_record(r2)
-cash_calculator.add_record(r3)
+    cash_calculator.add_record(r1)
+    cash_calculator.add_record(r2)
+    cash_calculator.add_record(r3)
 
-r4 = Record(amount=1500, comment='Кусок тортика.', date='16.07.2020')
-r5 = Record(amount=84, comment='Йогурт.')
-r6 = Record(amount=1140, comment='Баночка чипсов.', date='16.07.2020')
+    r4 = Record(amount=1500, comment="Тортик", date="23.07.2020")
+    r5 = Record(amount=84, comment="Йогурт.")
+    r6 = Record(amount=1140, comment="Баночка чипсов.", date="23.07.2020")
 
-calories_calculator.add_record(r4)
-calories_calculator.add_record(r5)
-calories_calculator.add_record(r6)
- 
-print(cash_calculator.get_today_stats())
-print(cash_calculator.get_week_stats())
-print(cash_calculator.get_today_cash_remained('eur'))
-print(cash_calculator.get_today_cash_remained('gdg'))
-print(calories_calculator.get_calories_remained())
-print(calories_calculator.get_week_stats())
-print(calories_calculator.get_today_stats())
+    calories_calculator.add_record(r4)
+    calories_calculator.add_record(r5)
+    calories_calculator.add_record(r6)
+
+    print(cash_calculator.get_today_stats())
+    print(cash_calculator.get_week_stats())
+    print(cash_calculator.get_today_cash_remained('eur'))
+    print(cash_calculator.get_today_cash_remained('gdg'))
+    print(calories_calculator.get_calories_remained())
+    print(calories_calculator.get_week_stats())
+    print(calories_calculator.get_today_stats())
+    print(calories_calculator.get_calories_remained())
